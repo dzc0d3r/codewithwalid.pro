3db0481f2b69206f68c80f34991b2110edf89dfc
Walid Lamraoui <m4dcod3r@protonmail.com>
2024-09-10 04:08:16 +0100

    feat(ui): minor changes in navbar links

--- packages/ui/layout/navbar/links.tsx
+++ packages/ui/layout/navbar/links.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { ucfirst } from "@codewithwalid/lib"
 import {
   AnimatePresence,
   motion,
@@ -36,77 +37,34 @@ const Links = ({ routes }: RoutesProps) => {
       setHash(href)
     }
   }
-  const mapRange = (
-    inputLower: number,
-    inputUpper: number,
-    outputLower: number,
-    outputUpper: number,
-  ) => {
-    const INPUT_RANGE = inputUpper - inputLower
-    const OUTPUT_RANGE = outputUpper - outputLower
-
-    return (value: number) =>
-      outputLower + (((value - inputLower) / INPUT_RANGE) * OUTPUT_RANGE || 0)
-  }
-  const setTransform = (
-    item: HTMLElement & EventTarget,
-    event: React.PointerEvent,
-    x: MotionValue,
-    y: MotionValue,
-  ) => {
-    const bounds = item.getBoundingClientRect()
-    const relativeX = event.clientX - bounds.left
-    const relativeY = event.clientY - bounds.top
-    const xRange = mapRange(0, bounds.width, -1, 1)(relativeX)
-    const yRange = mapRange(0, bounds.height, -1, 1)(relativeY)
-    x.set(xRange * 10)
-    y.set(yRange * 10)
-  }
 
   return (
-    <nav className="mx-auto hidden flex-1 justify-center lg:space-x-2 lg:flex">
+    <nav className="mx-auto hidden flex-1 justify-center lg:flex lg:space-x-2">
       {routes && routes.length > 0 ? (
-        <ul className="flex flex-row space-x-2">
+        <ul className="flex flex-row gap-1">
           <AnimatePresence>
             {routes.map((route, i) => {
               const x = useMotionValue(0)
               const y = useMotionValue(0)
-              const textX = useTransform(x, (latest) => latest * 0.5)
-              const textY = useTransform(y, (latest) => latest * 0.5)
 
               return (
-                <motion.li
-                  onPointerMove={(event) => {
-                    const item = event.currentTarget
-                    setTransform(item, event, x, y)
-                  }}
-                  key={route.href}
-                  onPointerLeave={(event) => {
-                    x.set(0)
-                    y.set(0)
-                  }}
-                  style={{ x, y }}
-                >
+                <motion.li key={route.href} style={{ x, y }}>
                   {route.href.startsWith("#") ? (
                     <MotionLink
                       key={i}
                       href={route.href}
                       onClick={() => handleClick(route.href)}
-                      className={`${route.showOnPath === path ? "flex" : "hidden"} ${buttonVariants(
-                        { variant: "ghost", size: "rounded" },
+                      className={`${route.showOnPath === path ? "flex items-center justify-center" : "hidden"} ${buttonVariants(
+                        { variant: "transparent", size: "rounded" },
                       )} md:text-normal relative text-sm transition-all duration-500 ease-in-out lg:text-base`}
                     >
-                      <motion.span
-                        style={{ x: textX, y: textY }}
-                        className={`relative z-30 ${route.href === hash ? "text-white" : null}`}
-                      >
-                        {route.label}
-                      </motion.span>
+                      {ucfirst(route.label)}
+
                       {hash === route.href ? (
                         <motion.div
                           transition={{ type: "spring" }}
-                          layoutId="underline"
-                          className={`${buttonVariants({ variant: "default", size: "rounded" })} absolute bottom-0 left-0 h-full w-full`}
+                          layoutId="active"
+                          className={`border-primary absolute -bottom-4 w-[50%] -translate-y-1/2 transform border-b-[.2rem]`}
                         ></motion.div>
                       ) : null}
                     </MotionLink>
@@ -114,22 +72,17 @@ const Links = ({ routes }: RoutesProps) => {
                     <MotionLink
                       key={i}
                       href={route.href}
-                      className={`${buttonVariants(route.href === path ? { variant: "default", size: "rounded" } : { variant: "ghost", size: "rounded" })} duration-400 md:text-normal relative text-sm transition-all ease-in-out lg:text-base`}
+                      className={`${buttonVariants({ variant: "transparent", size: "rounded" })} duration-400 md:text-normal relative text-sm transition-all ease-in-out first-letter:uppercase lg:text-base`}
                       target={
                         route.href.startsWith("https://") ? "_blank" : "_self"
                       }
                     >
-                      <motion.span
-                        style={{ x: textX, y: textY }}
-                        className="relative z-10"
-                      >
-                        {route.label}
-                      </motion.span>
+                      {ucfirst(route.label)}
                       {path === route.href ? (
                         <motion.div
                           transition={{ type: "spring" }}
-                          layoutId="underline"
-                          className={`${buttonVariants({ variant: "default", size: "rounded" })} absolute bottom-0 left-0 h-full w-full`}
+                          layoutId="active"
+                          className={`border-primary absolute -bottom-4 left-0 w-full -translate-x-1/2 transform border-b-[.2rem]`}
                         ></motion.div>
                       ) : null}
                     </MotionLink>
--- packages/ui/lib/utils.ts
+++ packages/ui/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export function ucfirst(string: string) {
+  if (!string) return;
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
